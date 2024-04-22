14756791847997b20f1bc4c2e6fe81a8d36ea3a8
OmmDigi <ommdigitalsolution@gmail.com>
2024-04-22 19:38:57 +0530

    fixed

--- frontend/app/components/NewCarousel.tsx
+++ frontend/app/components/NewCarousel.tsx
@@ -18,13 +18,14 @@ SwiperCore.use([Navigation]);
 
 interface IProps {
   children: React.ReactNode;
-  dataLength : number;
-  datas : ITours[]
+  dataLength: number;
+  datas: ITours[];
 }
 
 function NewCarousel({ children, dataLength, datas }: IProps) {
-  const swiperRef = useRef<typeof Swiper | null>(null);
+  const swiperRef = useRef<any>(null);
   const [currentIndex, setCurrentIndex] = useState(1);
+  const [sliderPreviewView, setSliderPreviewView] = useState(3);
 
   const onPrevBtnClicked = () => {
     setCurrentIndex((preIndex) => preIndex - 1);
@@ -35,8 +36,11 @@ function NewCarousel({ children, dataLength, datas }: IProps) {
   };
 
   useEffect(() => {
+    setSliderPreviewView(window?.innerWidth <= MOBILE_VIEW_WIDTH ? 1 : 3);
     if (currentIndex !== -1) {
-      swiperRef.current.swiper.slideTo(currentIndex);
+      if (swiperRef.current) {
+        swiperRef.current.swiper.slideTo(currentIndex);
+      }
     }
   }, [currentIndex]);
 
@@ -45,7 +49,7 @@ function NewCarousel({ children, dataLength, datas }: IProps) {
       <Swiper
         ref={swiperRef}
         spaceBetween={50}
-        slidesPerView={window?.innerWidth <= MOBILE_VIEW_WIDTH ? 1 : 3}
+        slidesPerView={sliderPreviewView}
         onSlideChange={() => console.log("slide change")}
         onSwiper={(swiper: any) => console.log(swiper)}
         centeredSlides={true}
@@ -60,44 +64,52 @@ function NewCarousel({ children, dataLength, datas }: IProps) {
         // }}
       >
         {datas.map((item, index) => (
-        <SwiperSlide key={item.ID}>
-          <li className={`toupPackagesShdow min-w-[365px] min-h-[365px] sm:min-w-full border border-[#e1e1e1] cursor-pointer ${currentIndex === index ? "scale-100" : "scale-75"} transition-all duration-1000`}>
-            <Link href={`/tour/${item.ID}`}>
-              <Image
-                className="w-full h-48 hover:scale-105 transition-all duration-500"
-                src={decodeURI(item.IMG)}
-                alt="img"
-                height={1200}
-                width={1200}
-              />
-              <div className="w-full flex flex-col items-center py-4 px-5">
-                <h2 className="text-center font-semibold">{item.TITLE}</h2>
-                <div className="flex items-center gap-3 my-2">
-                  <span className="text-gray-500 text-[14.3px]">From</span>
-                  <span className="font-extrabold text-[#2e66aa] text-2xl tracking-widest">
-                    {/* {₹} */}
-                    {item.PRICE}
-                  </span>
+          <SwiperSlide key={item.ID}>
+            <li
+              className={`toupPackagesShdow min-w-[365px] min-h-[365px] sm:min-w-full border border-[#e1e1e1] cursor-pointer ${
+                currentIndex === index ? "scale-100" : "scale-75"
+              } transition-all duration-1000`}
+            >
+              <Link href={`/tour/${item.ID}`}>
+                <Image
+                  className="w-full h-48 hover:scale-105 transition-all duration-500"
+                  src={decodeURI(item.IMG)}
+                  alt="img"
+                  height={1200}
+                  width={1200}
+                />
+                <div className="w-full flex flex-col items-center py-4 px-5">
+                  <h2 className="text-center font-semibold">{item.TITLE}</h2>
+                  <div className="flex items-center gap-3 my-2">
+                    <span className="text-gray-500 text-[14.3px]">From</span>
+                    <span className="font-extrabold text-[#2e66aa] text-2xl tracking-widest">
+                      {/* {₹} */}
+                      {item.PRICE}
+                    </span>
+                  </div>
+                  <div className="flex items-center gap-3">
+                    <BsClockHistory color="#2e66aa" />
+                    <span className="text-sm">{item.TIME}</span>
+                  </div>
                 </div>
-                <div className="flex items-center gap-3">
-                  <BsClockHistory color="#2e66aa" />
-                  <span className="text-sm">{item.TIME}</span>
-                </div>
-              </div>
-            </Link>
-          </li>
-        </SwiperSlide>
-      ))}
+              </Link>
+            </li>
+          </SwiperSlide>
+        ))}
       </Swiper>
 
       <button
-        className={`absolute top-1/2 left-4 z-20 transform -translate-y-1/2 homeInfoSection bg-white bg-opacity-80 text-gray-800 p-3 rounded-full focus:outline-none ${currentIndex <= 0 ? "hidden" : "block"}`}
+        className={`absolute top-1/2 left-4 z-20 transform -translate-y-1/2 homeInfoSection bg-white bg-opacity-80 text-gray-800 p-3 rounded-full focus:outline-none ${
+          currentIndex <= 0 ? "hidden" : "block"
+        }`}
         onClick={onPrevBtnClicked}
       >
         <GrPrevious />
       </button>
       <button
-        className={`absolute top-1/2 right-4 z-20 transform -translate-y-1/2 bg-white homeInfoSection bg-opacity-80 text-gray-800 p-3 rounded-full focus:outline-none ${currentIndex >= dataLength - 1 ? "hidden" : "block"}`}
+        className={`absolute top-1/2 right-4 z-20 transform -translate-y-1/2 bg-white homeInfoSection bg-opacity-80 text-gray-800 p-3 rounded-full focus:outline-none ${
+          currentIndex >= dataLength - 1 ? "hidden" : "block"
+        }`}
         onClick={onNextBtnClicked}
       >
         <GrNext />
