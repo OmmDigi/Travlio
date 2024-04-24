9af3dc22588d4febdb0d81cfd4df932abf740a34
OmmDigi <ommdigitalsolution@gmail.com>
2024-04-24 18:46:49 +0530

    fix filter for mobile

--- frontend/app/components/filters/MobileFIlterMenu.tsx
+++ frontend/app/components/filters/MobileFIlterMenu.tsx
@@ -1,6 +1,18 @@
 import React from "react";
 import Filter from "./Filter";
 
+const duration = [
+  { text: "15 Days 14 Nights", isChecked: false },
+  { text: "11 Days 10 Nights", isChecked: false },
+  { text: "10 Days 9 Nights", isChecked: false },
+  { text: "7 Days 6 Nights", isChecked: false },
+];
+
+const tourType = [
+  { text: "International Tour", isChecked: false },
+  { text: "Domestic Tour", isChecked: false },
+];
+
 function MobileFIlterMenu() {
   return (
     <div className="w-full h-full relative">
@@ -11,9 +23,8 @@ function MobileFIlterMenu() {
             Reset
           </span>
         </div>
-        <Filter name="Duration" filters={["1 Day Tour", "2-4 Dasy Tour"]} />
-        <Filter name="Month" filters={["1 Day Tour", "2-4 Dasy Tour"]} />
-        <Filter name="Destination" filters={["1 Day Tour", "2-4 Dasy Tour"]} />
+        <Filter name="Duration" filters={duration} />
+        <Filter name="Month" filters={tourType} />
       </div>
     </div>
   );
